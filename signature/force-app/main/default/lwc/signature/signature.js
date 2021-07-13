28a503b7dc78c01530fd0b7ec9426d044da8b1c9
Medha Bhargav Prabhala <mprabhala@salesforce.com>
2021-07-12 17:04:03 -0700

    Signature with Calendly

---
+++ signature/force-app/main/default/lwc/signature/signature.js
@@ -0,0 +1,47 @@
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import { NavigationMixin } from 'lightning/navigation';
+
+import SIGNATURE_FIELD from '@salesforce/schema/Signature__c';
+import NAME_FIELD from '@salesforce/schema/Signature__c.Name';
+import PHONE_FIELD from '@salesforce/schema/Signature__c.Phone__c';
+import MOBILE_PHONE_FIELD from '@salesforce/schema/Signature__c.Mobile_Phone__c';
+import EMAIL_FIELD from '@salesforce/schema/Signature__c.Email__c';
+
+import calendly from '@salesforce/resourceUrl/calendly';
+import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
+
+// const PROPERTY_FIELDS = [SIGNATURE_FIELD];
+const SIGNATURE_FIELDS = [
+    NAME_FIELD,
+    PHONE_FIELD,
+    MOBILE_PHONE_FIELD,
+    EMAIL_FIELD
+];
+
+export default class Signature extends NavigationMixin(LightningElement) {
+    @api recordId;
+
+    signatureFields = SIGNATURE_FIELDS;
+
+    // @wire(getRecord, { recordId: '$recordId', fields: PROPERTY_FIELDS })
+    // property;
+
+    get signatureId() {
+        return getFieldValue(this.recordId, SIGNATURE_FIELD);
+    }
+
+    //loadScript(this, calendly + '/widget.js').then(() => {
+    //    Calendly._autoLoadInlineWidgets();
+    //});
+    // handleNavigateToRecord() {
+    //     this[NavigationMixin.Navigate]({
+    //         type: 'standard__recordPage',
+    //         attributes: {
+    //             recordId: this.brokerId,
+    //             objectApiName: 'Property__c',
+    //             actionName: 'view'
+    //         }
+    //     });
+    // }
+}
